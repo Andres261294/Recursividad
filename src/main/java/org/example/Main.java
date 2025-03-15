3ad4f12643a656eddac95653d3cfa564d2f0b092
Julian Murcia <165083282+JulianD27@users.noreply.github.com>
2025-03-14 20:43:46 -0500

    otro cambio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,3 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el número de discos: ");
+        int n = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("Pasos para resolver la Torre de Hanoi con " + n + " discos:");
+        resolverTorreDeHanoi(n, 'A', 'C', 'B');
+    }
