f20053192640992b476c0eb08bc10ea9d6eeccd5
Jefredalarcón <jefredpacalagua@gmail.com>
2025-03-15 23:31:18 -0500

    rama

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,3 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el número de discos: ");
+
+
+        public static void resolverTorreDeHanoi(int n, char origen, char destino, char auxiliar) {
+            if (n == 1) {
+                System.out.println("Mover disco 1 de " + origen + " a " + destino);
+            } else {
+                resolverTorreDeHanoi(n - 1, origen, auxiliar, destino);
+                System.out.println("Mover disco " + n + " de " + origen + " a " + destino);
+                resolverTorreDeHanoi(n - 1, auxiliar, destino, origen);
+            }
+        }
+    }
